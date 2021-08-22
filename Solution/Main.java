a7b8b8844cbf11143e1a07477e9bc0e4cb697e32
codytheking <codytheking93@gmail.com>
2021-08-22 11:05:22 -0700

    Solution code

---
+++ Solution/Main.java
@@ -0,0 +1,21 @@
+/**
+ * This prints my initials in big letters.
+ * 
+ * @author Mr. King 
+ * @version 8/22/2021
+ */
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("    C C C C         J J J J J J J     K       K");
+        System.out.println("  C         C             J           K     K");
+        System.out.println("C                         J           K   K");
+        System.out.println("C                         J           K K");
+        System.out.println("C                         J           K   K");
+        System.out.println("  C         C       J     J           K     K");
+        System.out.println("    C C C C          J J J            K       K");
+    }
+}
+
